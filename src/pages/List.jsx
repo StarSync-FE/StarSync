30765b0d96482f5d34dcaf007326b9cd73277a99
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-04-16 16:15:18 +0900

    💄Style: 목록 페이지 이달의 차트 UI 추가
    # 커밋 메시지 형식
    # <이모지><타입>: <제목> (#이슈 번호)
    #
    # ⛔ 제목은 100자 이내, 끝에 마침표(.) 사용 금지
    # ⛔ 이모지와 타입 사이에 공백 없이 붙여 씁니다 (예: ✨Feat)
    # ⛔ 제목과 콜론(:) 사이에 공백 없이 붙이고, 콜론 뒤에는 공백 1칸 필요
    
    # ✅ 제목은 커밋의 "무엇"을 나타냅니다
    # ✅ 본문은 "왜", "어떻게"를 요약하며, 선택사항입니다
    # ✅ 본문은 여러 줄의 메시지를 작성할 땐 "-"로 구분
    
    # 예시:
    # ✨Feat: 마이페이지 수정 기능 추가 (#15)
    
    # 커밋 타입 가이드:
    # ✨ Feat     : 새로운 기능 추가
    # 🐛 Fix      : 버그 수정
    # 📦 Chore    : 설정/빌드 수정, 기타 잡일
    # 💄 Style    : 포맷팅, 세미콜론 등 비즈니스 로직 없는 변경
    # 📝 Docs     : 문서 작성 및 수정
    # ♻️ Refactor : 리팩토링 (동작 변경 없이 코드 개선)

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,3 +1,19 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+const girls = [
+  { name: '아이브', img: 'https://example.com/ive.jpg', id: 1 },
+  { name: '뉴진스', img: 'https://example.com/newjeans.jpg', id: 2 },
+  { name: '블랙핑크', img: 'https://example.com/blackpink.jpg', id: 3 },
+  { name: '트와이스', img: 'https://example.com/twice.jpg', id: 4 },
+  { name: '레드벨벳', img: 'https://example.com/redvelvet.jpg', id: 5 },
+  { name: '소녀시대', img: 'https://example.com/girlsgeneration.jpg', id: 6 },
+  { name: '에스파', img: 'https://example.com/aespa.jpg', id: 7 },
+  { name: '여자친구', img: 'https://example.com/gfriend.jpg', id: 8 },
+  { name: '있지', img: 'https://example.com/itzy.jpg', id: 9 },
+  { name: '오마이걸', img: 'https://example.com/ohmygirl.jpg', id: 10 },
+];
+
 function List() {
   return (
     <>
@@ -5,6 +21,25 @@ function List() {
         <p>내 크레딧</p>
         <img src="" alt="크레딧" />
         <button type="button">충전하기</button>
+      </div>
+      <div>캐러셀</div>
+      <div>
+        <div>
+          <h1>이달의 차트</h1>
+          <p>차트 투표하기</p>
+        </div>
+        <div>
+          <div>이달의 여자 아이돌</div>
+          <div>이달의 남자 아이돌</div>
+          <ul>
+            {girls.map((name, img) => (
+              <li key={name.id}>
+                <img src={name.img} alt={name.name} />
+                <p>{name.name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
