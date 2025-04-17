7d299db8f707c295d2b230ce33b8905bb2bb2246
Joinsung <leeming01@naver.com>
2025-04-17 16:47:08 +0900

    📝Docs: media 유틸에 JSDoc 주석 추가
    # 커밋 메시지 형식
    # <이모지><타입>: <제목> (#이슈 번호)
    #
    # ⛔ 제목은 100자 이내, 끝에 마침표(.) 사용 금지
    # ⛔ 이모지와 타입 사이에 공백 없이 붙여 씁니다 (예: ✨Feat)
    # ⛔ 제목과 콜론(:) 사이에 공백 없이 붙이고, 콜론 뒤에는 공백 1칸 필요
    
    # ✅ 제목은 커밋의 "무엇"을 나타냅니다
    # ✅ 본문은 "왜", "어떻게"를 요약하며, 선택사항입니다
    # ✅ 본문은 여러 줄의 메시지를 작성할 땐 "-"로 구분
    
    # 예시:
    # ✨Feat: 마이페이지 수정 기능 추가 (#15)
    
    # 커밋 타입 가이드:
    # ✨ Feat     : 새로운 기능 추가
    # 🐛 Fix      : 버그 수정
    # 📦 Chore    : 설정/빌드 수정, 기타 잡일
    # 💄 Style    : 포맷팅, 세미콜론 등 비즈니스 로직 없는 변경
    # 📝 Docs     : 문서 작성 및 수정
    # ♻️ Refactor : 리팩토링 (동작 변경 없이 코드 개선)

--- src/styles/responsive.js
+++ src/styles/responsive.js
@@ -1,6 +1,16 @@
 import facepaint from 'facepaint';
 
 const breakpoints = [375, 744, 1024, 1280];
+/**
+ * `facepaint`를 사용하여 브레이크포인트에 맞는 미디어 쿼리를 반환하는 함수
+ *
+ * @param {object} styles - 스타일 객체 (예: padding, font-size 등)
+ * @returns {string} - 각 브레이크포인트에 대응하는 미디어 쿼리와 함께 적용될 스타일
+ *
+ * @example
+ * media({ padding: ['10px', '20px', '30px', '40px'] })
+ * // 10px는 기본 값 375px 이상에서는 padding: 20px, 744px 이상에서는 padding: 30px, 1024px 이상에서는 padding: 40px이 적용 됩니다.
+ */
 const media = facepaint(breakpoints.map((bp) => `@media (min-width: ${bp}px)`));
 
 export default media;
