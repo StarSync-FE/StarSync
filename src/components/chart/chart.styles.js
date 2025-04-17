2e7777811d3ea1d9e70e1fec92c279dac08e16bc
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-04-18 02:45:52 +0900

    💄Style: 이달의 차트 UI 구체화
    # 커밋 메시지 형식
    # <이모지><타입>: <제목> (#이슈 번호)
    #
    # ⛔ 제목은 100자 이내, 끝에 마침표(.) 사용 금지
    # ⛔ 이모지와 타입 사이에 공백 없이 붙여 씁니다 (예: ✨Feat)
    # ⛔ 제목과 콜론(:) 사이에 공백 없이 붙이고, 콜론 뒤에는 공백 1칸 필요
    
    # ✅ 제목은 커밋의 "무엇"을 나타냅니다
    # ✅ 본문은 "왜", "어떻게"를 요약하며, 선택사항입니다
    # ✅ 본문은 여러 줄의 메시지를 작성할 땐 "-"로 구분
    
    # 예시:
    # ✨Feat: 마이페이지 수정 기능 추가 (#15)
    
    # 커밋 타입 가이드:
    # ✨ Feat     : 새로운 기능 추가
    # 🐛 Fix      : 버그 수정
    # 📦 Chore    : 설정/빌드 수정, 기타 잡일
    # 💄 Style    : 포맷팅, 세미콜론 등 비즈니스 로직 없는 변경
    # 📝 Docs     : 문서 작성 및 수정
    # ♻️ Refactor : 리팩토링 (동작 변경 없이 코드 개선)

--- src/components/chart/chart.styles.js
+++ src/components/chart/chart.styles.js
@@ -11,20 +11,21 @@ export const chartSection = css`
 
 export const chartSectionHeader = css`
   display: flex;
+  align-items: center;
   justify-content: space-between;
 
   div {
+    font-size: 1.5rem;
     font-weight: 700;
-  
   }
 
   button {
     width: 8rem;
     height: 2rem;
     border: none;
-    border-radius: 0.5rem;
+    border-radius: 0.3rem;
     font-size: 0.8rem;
-    font-weight: 700;
+    font-weight: 900;
     color: white;
     background-color: #F96D69;
     cursor: pointer;
@@ -33,38 +34,51 @@ export const chartSectionHeader = css`
     }
 
     div {
+    font-size: 0.75rem;
     display: flex;
     align-items: center;
-    margin-left: 0.3rem;
+    justify-content: center;
     gap: 0.4rem;
 
     img {
     width: 1rem;
     height: 1rem;
     }
+
+    span {
+      font-weight: 900;
+    }
   }
 }
 `;
 
 export const tabButtonWrapper = css`
+  margin-top: 2rem;
   display: flex;
+  align-items: center;
   justify-content: center;
-  gap: 1rem;
+
+  button {
+    padding: 1.5rem 15rem;
+  }
 `;
 
 export const idolListButton = css`
-    margin-top: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 4rem;
+    margin: 0 auto;
     background-color: black;
     color: white;
     border: none;
     border-radius: 0.2rem;
-    padding: 1rem 10.5rem;
+    padding: 1rem 0;
     font-size: 1rem;
     cursor: pointer;
-    
-    div {
-      width: 7.5rem;
-    }
+    text-align: center;
+    white-space: nowrap;
 `;
 
 export const activeButton = css`
@@ -74,6 +88,9 @@ export const activeButton = css`
 
 export const idolList = css`
   margin-top: 1rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1.2rem;
 
   li {
     margin-bottom: 1rem;
@@ -92,7 +109,8 @@ export const idolList = css`
     img {
       width: 4rem;
       height: 4rem;
-      border: 2px solid #ddd;
+      border: 2px solid white;
+      box-shadow: 0 0 0 0.1rem #F96D69;
       border-radius: 50%;
       object-fit: cover;
     }
@@ -100,9 +118,9 @@ export const idolList = css`
 `;
 
 export const moreButton = css`
+  font-size: 1.2rem;
   display: flex;
   margin: 2rem auto;
-  padding-top: 1rem;
   background-color: black;
   color: white;
   border: 1px solid white;
