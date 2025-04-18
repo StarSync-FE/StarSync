df82b7cdb9a3dbf593b03cff33706400ebf143ca
Joinsung <leeming01@naver.com>
2025-04-18 16:07:42 +0900

    📝Docs: 버튼 컴포넌트 JSDoc 작성
    # 커밋 메시지 형식
    # <이모지><타입>: <제목> (#이슈 번호)
    #
    # ⛔ 제목은 100자 이내, 끝에 마침표(.) 사용 금지
    # ⛔ 이모지와 타입 사이에 공백 없이 붙여 씁니다 (예: ✨Feat)
    # ⛔ 제목과 콜론(:) 사이에 공백 없이 붙이고, 콜론 뒤에는 공백 1칸 필요
    
    # ✅ 제목은 커밋의 "무엇"을 나타냅니다
    # ✅ 본문은 "왜", "어떻게"를 요약하며, 선택사항입니다
    # ✅ 본문은 여러 줄의 메시지를 작성할 땐 "-"로 구분
    
    # 예시:
    # ✨Feat: 마이페이지 수정 기능 추가 (#15)
    
    # 커밋 타입 가이드:
    # ✨ Feat     : 새로운 기능 추가
    # 🐛 Fix      : 버그 수정
    # 📦 Chore    : 설정/빌드 수정, 기타 잡일
    # 💄 Style    : 포맷팅, 세미콜론 등 비즈니스 로직 없는 변경
    # 📝 Docs     : 문서 작성 및 수정
    # ♻️ Refactor : 리팩토링 (동작 변경 없이 코드 개선)

--- src/components/customButton/CustomButton.jsx
+++ src/components/customButton/CustomButton.jsx
@@ -2,6 +2,33 @@
 import { css } from '@emotion/react';
 import getButtonStyle from './customButton.styles';
 
+/**
+ * CustomButton 컴포넌트는 다양한 스타일과 크기를 가진 버튼을 렌더링합니다.
+ * 각기 다른 버튼 유형에 따라 크기 및 스타일을 동적으로 변경할 수 있습니다.
+ *
+ * @param {string} [type='button'] 버튼의 유형. 기본값은 'button'입니다.
+ * @param {('modal'|'modalChart'|'carousel'|'vote'|'landing')} [variant='modal'] 버튼의 스타일 변형을 정의하는 속성입니다.
+ * @param {boolean} [isRound=false] 버튼을 둥근 형태로 만들지 여부를 결정합니다.
+ * @param {boolean} [disabled=false] 버튼의 비활성화 상태를 설정합니다.
+ * @param {Object} [style] 버튼에 추가적인 CSS 스타일을 적용할 수 있습니다.
+ * @param {Function} [onClick] 버튼이 클릭될 때 실행되는 클릭 이벤트 핸들러 함수입니다.
+ * @param {ReactNode} children 버튼 내부에 렌더링할 자식 요소입니다.
+ *
+ * @returns {JSX.Element} CustomButton 컴포넌트의 JSX 요소
+ *
+ *  @example
+ * // 버튼을 클릭 시 알림을 띄우는 예시
+ * <CustomButton
+ *   type="submit"
+ *   variant="carousel"
+ *   isRound={true} //bool 값일 경우 true면 isRound로 사용해도 무방
+ *   disabled={false}
+ *   onClick={() => alert('버튼 클릭!')}
+ * >
+ *   클릭하세요
+ * </CustomButton>
+ */
+
 const CustomButton = ({
   type = 'button',
   variant = 'modal',
