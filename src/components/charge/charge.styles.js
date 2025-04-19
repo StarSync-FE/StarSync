d7cfa05b65fe856ee423d32b1ac6d0a8594df450
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-04-19 12:39:22 +0900

    💄Style: rem 단위 통합 및 글로벌 스타일 적용
    # 커밋 메시지 형식
    # <이모지><타입>: <제목> (#이슈 번호)
    #
    # ⛔ 제목은 100자 이내, 끝에 마침표(.) 사용 금지
    # ⛔ 이모지와 타입 사이에 공백 없이 붙여 씁니다 (예: ✨Feat)
    # ⛔ 제목과 콜론(:) 사이에 공백 없이 붙이고, 콜론 뒤에는 공백 1칸 필요
    
    # ✅ 제목은 커밋의 "무엇"을 나타냅니다
    # ✅ 본문은 "왜", "어떻게"를 요약하며, 선택사항입니다
    # ✅ 본문은 여러 줄의 메시지를 작성할 땐 "-"로 구분
    
    # 예시:
    # ✨Feat: 마이페이지 수정 기능 추가 (#15)
    
    # 커밋 타입 가이드:
    # ✨ Feat     : 새로운 기능 추가
    # 🐛 Fix      : 버그 수정
    # 📦 Chore    : 설정/빌드 수정, 기타 잡일
    # 💄 Style    : 포맷팅, 세미콜론 등 비즈니스 로직 없는 변경
    # 📝 Docs     : 문서 작성 및 수정
    # ♻️ Refactor : 리팩토링 (동작 변경 없이 코드 개선)

--- src/components/charge/charge.styles.js
+++ src/components/charge/charge.styles.js
@@ -6,8 +6,8 @@ export const creditWrapper = css`
   justify-content: space-between;
   padding: 2rem;
   margin: 2rem 0;
-  border: 1px solid white;
-  border-radius: 8px;
+  border: 1px solid var(--white);
+  border-radius: 0.5rem;
   font-weight: 700;
 
   div {
@@ -18,11 +18,11 @@ export const creditWrapper = css`
     width: 5rem;
     height: 3rem;
     border: none;
-    border-radius: 8px;
+    border-radius: 0.5rem;
     font-size: 0.8rem;
     font-weight: 700;
-    color: #F96D69;
-    background-color: black;
+    color: var(--orange);
+    background-color: var(--black);
     cursor: pointer;
   }
 `;
@@ -32,19 +32,34 @@ export const credit = css`
   align-items: center;
   gap: 0.5rem;
   margin-top: 0.5rem;
-  border-radius: 8px;
-  font-size: 14px;
+  border-radius: .5rem;
+  font-size: .9rem;
   font-weight: 500;
 
   img {
     margin-top: 0.2rem;
-    width: 24px;
-    height: 24px;
+    width: 1.5rem;
+    height: 1.5rem;
   }
 
   span {
     font-size: 18px;
     font-weight: 700;
-    color: #F96D69;
+    color: var(--orange);
+  }
+`;
+
+export const radioButtonContent = css`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  img {
+    width: 1rem;
+    height: 1rem;
+  }
+
+  span {
+    font-weight: 700;
   }
 `;
