c6c09daa55ccf8f4ed80c905454f2e64adff9a07
Joinsung <leeming01@naver.com>
2025-04-19 15:23:56 +0900

    ♻️Refactor: 색상 변수 추가 및 일부 스타일 수정

--- src/styles/global.styles.jsx
+++ src/styles/global.styles.jsx
@@ -5,11 +5,13 @@ const resetStyle = css`
 :root {
   --black: #181D26; 
   --black-deep: #02000E;
+  --black-full: #000000;
   --gray: #828282;
   --gray-dark: #67666E;
   --gray-light: #A3A5A8;
   --gray-cool: #8C92AB;
   --white: #F7F7F8;
+  --white-full: #FFFFFF;
   --orange: #F96D69;
   --pink: #FE5493;
   --pink-bright: #ED56C2;
@@ -17,16 +19,16 @@ const resetStyle = css`
   --blue-sky: #1C9EFB;
   --blue-dark: #3D2FA9;
   --purple: #8D42FB;
-}
-
-html {
-  box-sizing: border-box;
-  font-size: 62.5%; /* 1rem = 10px */
-  font-family: 'Pretendard', sans-serif;
+  --light-green: #A6FF4D;
 }
 
 *, *::before, *::after {
-  box-sizing: inherit;
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%; /* 1rem = 10px */
+  font-family: 'Pretendard', sans-serif;
 }
 
 body {
@@ -36,14 +38,14 @@ body {
   background: var(--black-deep);
   -webkit-font-smoothing: antialiased; /* 텍스트를 부드럽게 렌더링 (WebKit) */
   -moz-osx-font-smoothing: grayscale;  /* 텍스트를 부드럽게 렌더링 (macOS Firefox) */
-  line-height: 1.4;
+  line-height: 1;
 }
 
 ul, ol, li {
   list-style: none;
 }
 
-a, button {
+a{
   color: inherit;
   text-decoration: none;
   cursor: pointer;
@@ -58,7 +60,8 @@ img {
 label,
 input,
 select,
-textarea {
+textarea,
+button {
   font: inherit;
   color: inherit;
   border: none;
@@ -66,6 +69,10 @@ textarea {
   box-shadow: none;
 }
 
+button {
+  background: none;
+  cursor: pointer;
+}
 
 h1, h2, h3, h4, h5, h6,
 p, span, strong,
