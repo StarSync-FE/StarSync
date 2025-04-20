af0398d1ab668a59646d98668fa7419bd950c0f2
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-04-21 08:57:34 +0900

    💄Style: 모달 창 기본 style 세팅
    # 커밋 메시지 형식
    # <이모지><타입>: <제목> (#이슈 번호)
    #
    # ⛔ 제목은 100자 이내, 끝에 마침표(.) 사용 금지
    # ⛔ 이모지와 타입 사이에 공백 없이 붙여 씁니다 (예: ✨Feat)
    # ⛔ 제목과 콜론(:) 사이에 공백 없이 붙이고, 콜론 뒤에는 공백 1칸 필요
    
    # ✅ 제목은 커밋의 "무엇"을 나타냅니다
    # ✅ 본문은 "왜", "어떻게"를 요약하며, 선택사항입니다
    # ✅ 본문은 여러 줄의 메시지를 작성할 땐 "-"로 구분
    
    # 예시:
    # ✨Feat: 마이페이지 수정 기능 추가 (#15)
    
    # 커밋 타입 가이드:
    # ✨ Feat     : 새로운 기능 추가
    # 🐛 Fix      : 버그 수정
    # 📦 Chore    : 설정/빌드 수정, 기타 잡일
    # 💄 Style    : 포맷팅, 세미콜론 등 비즈니스 로직 없는 변경
    # 📝 Docs     : 문서 작성 및 수정
    # ♻️ Refactor : 리팩토링 (동작 변경 없이 코드 개선)

--- src/components/modal/modal.styles.js
+++ src/components/modal/modal.styles.js
@@ -30,15 +30,11 @@ export const overlay = css`
   width: 100%;
   height: 100%;
   background-color: rgb(0 0 0 / 70%);
-  backdrop-filter: blur(0.8px); /* 배경 흐림 효과 */
+  backdrop-filter: blur(0.5px); /* 배경 흐림 효과 */
 `;
 
 export const container = css`
   position: relative;
-  width: 90%;
-  max-width: 525px;
-  height: 693px;
-  max-height: calc(100% - 4rem);
   padding: 2rem;
   border-radius: 8px;
   background-color: var(--black);
