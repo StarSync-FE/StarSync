17f8ac8a76a6f4b830d24f819a3fea1db64d9b77
sonsujin <sonsj33@gmail.com>
2025-04-22 03:50:08 +0900

    ✨Feat: 마이페이지 기초 ui 작업
    # 커밋 메시지 형식
    # <이모지><타입>: <제목> (#이슈 번호)
    #
    # ⛔ 제목은 100자 이내, 끝에 마침표(.) 사용 금지
    # ⛔ 이모지와 타입 사이에 공백 없이 붙여 씁니다 (예: ✨Feat)
    # ⛔ 제목과 콜론(:) 사이에 공백 없이 붙이고, 콜론 뒤에는 공백 1칸 필요
    
    # ✅ 제목은 커밋의 "무엇"을 나타냅니다
    # ✅ 본문은 "왜", "어떻게"를 요약하며, 선택사항입니다
    # ✅ 본문은 여러 줄의 메시지를 작성할 땐 "-"로 구분
    
    # 예시:
    # ✨Feat: 마이페이지 수정 기능 추가 (#15)
    
    # 커밋 타입 가이드:
    # ✨ Feat     : 새로운 기능 추가
    # 🐛 Fix      : 버그 수정
    # 📦 Chore    : 설정/빌드 수정, 기타 잡일
    # 💄 Style    : 포맷팅, 세미콜론 등 비즈니스 로직 없는 변경
    # 📝 Docs     : 문서 작성 및 수정
    # ♻️ Refactor : 리팩토링 (동작 변경 없이 코드 개선)

--- src/pages/my/MyPage.jsx
+++ src/pages/my/MyPage.jsx
@@ -1,15 +1,53 @@
 import Avatar from '@/components/avatar';
-import { css } from '@emotion/react';
-
+import AvatarButton from '@/components/avatarButton';
+import CustomButton from '@/components/customButton';
+import mockData from '@/data/mockData';
+import { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import * as S from './myPage.styles';
 const MyPage = () => {
+  const idols = useLoaderData(); // 여기서 데이터 받음
+  const [myIdol, setMyIdol] = useState([]);
+  const [allIdols, setAllIdols] = useState(idols.list);
+
+  const addMyIdol = (idol) => {
+    console.log(idol);
+    setMyIdol([...myIdol, idol]);
+  };
+
+  // useEffect(() => {
+  //   allIdols.filter((idol, index, arr) => {
+  //     return !myIdol.some((selected) => selected.id === idol.id);
+  //   });
+  // }, [myIdol, allIdols]);
+
   return (
     <div>
-      <section>
-        <h2>내가 관심있는 아이돌</h2>
-        <div>
-          <Avatar />
-        </div>
+      <h2>내가 관심있는 아이돌</h2>
+      <section css={S.horizonList}>
+        {myIdol.map((idol) => {
+          return (
+            <div key={idol.id}>
+              <AvatarButton size={6} imgUrl={idol.profilePicture} />
+              <h3 css={S.idolName}>{idol.name}</h3>
+              <p css={S.groupName}>{idol.group}</p>
+            </div>
+          );
+        })}
       </section>
+      <h2>관심 있는 아이돌을 추가해보세요.</h2>
+      <section>
+        {allIdols.map((idol) => {
+          return (
+            <div key={idol.id}>
+              <Avatar imgUrl={idol.profilePicture} onSelectToggle={() => addMyIdol(idol)} />
+              <h3 css={S.idolName}>{idol.name}</h3>
+              <p css={S.groupName}>{idol.group}</p>
+            </div>
+          );
+        })}
+      </section>
+      <CustomButton>추가하기</CustomButton>
     </div>
   );
 };
--- src/pages/my/myPage.styles.js
+++ src/pages/my/myPage.styles.js
@@ -1,1 +1,25 @@
 import { css } from '@emotion/react';
+
+export const horizonList = css`
+  display: flex;
+  overflow-x: scroll;
+  gap: 3.2rem;
+  width: 100%;
+  min-height: 123px;
+  padding: 3.2rem 2.4rem;
+`;
+
+export const idolName = css`
+  margin: 0.8rem 0 0.2rem;
+  font-size: 1.6rem;
+  font-weight: 700;
+  text-align: center;
+  color: var(--white);
+`;
+export const groupName = css`
+  margin: 0.2rem;
+  font-size: 1.4rem;
+  font-weight: 400;
+    text-align: center;
+  color: #FFF9;
+`;
