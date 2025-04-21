10aaa3ad819940fbf8d6399f003fb123b9cffb0c
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-04-22 05:19:23 +0900

    💄Style: figma 디자인에 맞게 모달 기초 디자인 수정
    # 커밋 메시지 형식
    # <이모지><타입>: <제목> (#이슈 번호)
    #
    # ⛔ 제목은 100자 이내, 끝에 마침표(.) 사용 금지
    # ⛔ 이모지와 타입 사이에 공백 없이 붙여 씁니다 (예: ✨Feat)
    # ⛔ 제목과 콜론(:) 사이에 공백 없이 붙이고, 콜론 뒤에는 공백 1칸 필요
    
    # ✅ 제목은 커밋의 "무엇"을 나타냅니다
    # ✅ 본문은 "왜", "어떻게"를 요약하며, 선택사항입니다
    # ✅ 본문은 여러 줄의 메시지를 작성할 땐 "-"로 구분
    
    # 예시:
    # ✨Feat: 마이페이지 수정 기능 추가 (#15)
    
    # 커밋 타입 가이드:
    # ✨ Feat     : 새로운 기능 추가
    # 🐛 Fix      : 버그 수정
    # 📦 Chore    : 설정/빌드 수정, 기타 잡일
    # 💄 Style    : 포맷팅, 세미콜론 등 비즈니스 로직 없는 변경
    # 📝 Docs     : 문서 작성 및 수정
    # ♻️ Refactor : 리팩토링 (동작 변경 없이 코드 개선)

--- src/components/modals/creditChargeModal/creditChargeModal.styles.js
+++ src/components/modals/creditChargeModal/creditChargeModal.styles.js
@@ -4,28 +4,47 @@ export const modalContent = css`
   display: flex;
   flex-direction: column;
   gap: 1rem;
-  width: 100%;
+  width: 20rem;
   max-width: 30rem;
-  padding: 2rem;
+  height: 22rem;
+
+  h2 {
+    margin: 0.5rem 0;
+    font-size: 1.125rem;
+  }
+
+  button {
+    width: 100%;
+    margin: 0.5rem 0;
+    border: none;
+    border-radius: 4px;
+    font-size: 1rem;
+    color: white;
+    background-color: var(--orange);
+    transition: background-color 0.3s ease;
+  }
 `;
 
 export const radioButtons = css`
     display: flex;
     flex-direction: column;
-    gap: 1rem;
-    padding: 1rem;
-    border: 1px solid #ccc;
-    border-radius: 8px;
+    gap: 0.4rem;
+    padding: 0.1rem;
     font-size: 1rem;
     font-weight: 500;
     cursor: pointer;
+
+    label {
+      padding: 1rem;
+      border: 1px solid #ccc;
+      border-radius: 8px;
+    }
 `;
 
 export const radioButtonContent = css`
   display: flex;
-  justify-content: space-between;
   align-items: center;
-  gap: 0.5rem;
+  gap: 0.3rem;
 
   img {
     width: 2rem;
@@ -36,4 +55,8 @@ export const radioButtonContent = css`
     font-size: 1.2rem;
     font-weight: 700;
   }
+`;
+
+export const buttonStyle = css` 
+  height: 3rem;
 `;
