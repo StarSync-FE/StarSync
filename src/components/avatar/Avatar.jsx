fa95989049c2ec314099f69bc1a3b9202ed47239
sonsujin <sonsj33@gmail.com>
2025-04-21 09:37:38 +0900

    ✨Feat: Avatar 공통 컴포넌트 ui 작업
    # 커밋 메시지 형식
    # <이모지><타입>: <제목> (#이슈 번호)
    #
    # ⛔ 제목은 100자 이내, 끝에 마침표(.) 사용 금지
    # ⛔ 이모지와 타입 사이에 공백 없이 붙여 씁니다 (예: ✨Feat)
    # ⛔ 제목과 콜론(:) 사이에 공백 없이 붙이고, 콜론 뒤에는 공백 1칸 필요
    
    # ✅ 제목은 커밋의 "무엇"을 나타냅니다
    # ✅ 본문은 "왜", "어떻게"를 요약하며, 선택사항입니다
    # ✅ 본문은 여러 줄의 메시지를 작성할 땐 "-"로 구분
    
    # 예시:
    # ✨Feat: 마이페이지 수정 기능 추가 (#15)
    
    # 커밋 타입 가이드:
    # ✨ Feat     : 새로운 기능 추가
    # 🐛 Fix      : 버그 수정
    # 📦 Chore    : 설정/빌드 수정, 기타 잡일
    # 💄 Style    : 포맷팅, 세미콜론 등 비즈니스 로직 없는 변경
    # 📝 Docs     : 문서 작성 및 수정
    # ♻️ Refactor : 리팩토링 (동작 변경 없이 코드 개선)

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,8 +1,19 @@
+import { useState } from 'react';
 /** @jsxImportSource @emotion/react */
 import * as S from './avatar.styles';
 
-const Avatar = ({ imgUrl }) => {
-  return <img src={imgUrl} alt="" css={S.image} />;
+const Avatar = ({ imgUrl, isSelected }) => {
+  return (
+    <>
+      <div
+        css={[isSelected && S.imageSelected, S.imageWrapper]}
+        onClick={() => setIsSelected(true)}
+        onKeyDown={() => setIsSelected(true)}
+      >
+        <img src={imgUrl} alt="" css={S.image} />
+      </div>
+    </>
+  );
 };
 
 export default Avatar;
--- src/components/avatar/avatar.styles.js
+++ src/components/avatar/avatar.styles.js
@@ -1,5 +1,28 @@
+import checkIcon from '@/assets/icons/check-icon.png';
 import { css } from '@emotion/react';
-
+export const imageWrapper = css`
+  user-select: none;
+  position: relative;
+`;
+export const imageSelected = css`
+  &::after {
+    content:"";
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    border: 4px solid var(--black);
+    border-radius: 50%;
+    width: 60px;
+    height: 60px;
+    box-sizing: border-box;
+    background-color: var(--orange);
+    opacity: 0.5;
+    background-image: url(${checkIcon});
+    background-size: 50%;
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+`;
 export const image = css`
   border-radius: 50%;
   width: 60px;
@@ -8,4 +31,6 @@ export const image = css`
   box-shadow: 0 0 0 1px var(--orange);
   box-sizing: border-box;
   object-fit: cover;
+
+  
 `;
