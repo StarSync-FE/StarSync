e2d2491fb69fdad51627d9ea215c88b23b1a4c4a
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-04-21 09:11:22 +0900

    ♻️Refactor: 크레딧 충전하기 모달 컴포넌트 분리

--- src/components/charge/Charge.jsx
+++ src/components/charge/Charge.jsx
@@ -1,14 +1,8 @@
 import credit from '@/assets/images/credit.jpg';
-import mockData from '@/data/mockData';
 import addCommas from '@/utils/addCommas';
-import { useState } from 'react';
-import Modal from '../modal';
-import RadioButton from '../radioButton';
 import * as S from './charge.styles';
 
-const Charge = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false); // 모달 상태 관리
-
+const Charge = ({ setModalType }) => {
   return (
     <div css={S.creditWrapper}>
       <div>
@@ -18,27 +12,9 @@ const Charge = () => {
           <span>{addCommas(1000000)}</span>
         </div>
       </div>
-      <button type="button" onClick={() => setIsModalOpen(true)}>
+      <button type="button" onClick={() => setModalType('creditCharge')}>
         충전하기
       </button>
-      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-        <div css={S.modalContent}>
-          <h2>크레딧 충전하기</h2>
-          <div css={S.radioButtons}>
-            {mockData.prices.map((price) => (
-              <RadioButton key={price.id} name="charge" priceLabel={price.name} isSelected={false}>
-                <div css={S.radioButtonContent}>
-                  <img src={credit} alt="크레딧" />
-                  <span>{price.value}</span>
-                </div>
-              </RadioButton>
-            ))}
-          </div>
-          <button type="button" onClick={() => setIsModalOpen(false)}>
-            충전하기
-          </button>
-        </div>
-      </Modal>
     </div>
   );
 };
