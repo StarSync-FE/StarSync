7e5c20549b154e86e8c76cfa17a9e017c062f845
Sienna <sgoldenbird@gmail.com>
2025-04-22 17:23:00 +0900

    💄Style: PendingUI 스타일링
    # 커밋 메시지 형식
    # <이모지><타입>: <제목> (#이슈 번호)
    #
    # ⛔ 제목은 100자 이내, 끝에 마침표(.) 사용 금지
    # ⛔ 이모지와 타입 사이에 공백 없이 붙여 씁니다 (예: ✨Feat)
    # ⛔ 제목과 콜론(:) 사이에 공백 없이 붙이고, 콜론 뒤에는 공백 1칸 필요
    
    # ✅ 제목은 커밋의 "무엇"을 나타냅니다
    # ✅ 본문은 "왜", "어떻게"를 요약하며, 선택사항입니다
    # ✅ 본문은 여러 줄의 메시지를 작성할 땐 "-"로 구분
    
    # 예시:
    # ✨Feat: 마이페이지 수정 기능 추가 (#15)
    
    # 커밋 타입 가이드:
    # ✨ Feat     : 새로운 기능 추가
    # 🐛 Fix      : 버그 수정
    # 📦 Chore    : 설정/빌드 수정, 기타 잡일
    # 💄 Style    : 포맷팅, 세미콜론 등 비즈니스 로직 없는 변경
    # 📝 Docs     : 문서 작성 및 수정
    # ♻️ Refactor : 리팩토링 (동작 변경 없이 코드 개선)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet, useNavigation } from 'react-router-dom';
+import { PendingUI } from './components/loadingStatus';
 
 function App() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
+  console.log('isLoading:', isLoading);
+  console.log('Navigation State:', navigation.state);
+
   return (
-    <main>
-      <Outlet />
-    </main>
+    <>
+      {isLoading && <PendingUI />}
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/list">List</Link>
+        <Link to="/mypage">My Page</Link>
+      </nav>
+
+      <main>
+        <Outlet />
+      </main>
+    </>
   );
 }
 
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -4,83 +4,75 @@ import { PendingUI } from '@/components/loadingStatus';
 import { ENDPOINTS } from '@/constants/api';
 import { NotFoundPage } from '@/pages/error';
 import { fetchData } from '@/utils/api';
-import { lazy } from 'react';
+import { Suspense, lazy } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
 const LandingPage = lazy(() => import('@/pages/landing/LandingPage'));
 const ListPage = lazy(() => import('@/pages/list/ListPage'));
 const MyPage = lazy(() => import('@/pages/mypage/MyPage'));
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      loader: async () => {
+        const idols = await fetchData(ENDPOINTS.GET_IDOLS);
+        return idols;
+      },
+      pendingElement: <PendingUI />,
+      errorElement: <ErrorBoundary />,
+      children: [
+        {
+          index: true,
+          element: <LandingPage />,
+        },
+        {
+          path: 'list',
+          element: <ListPage />,
+          loader: async () => {
+            const gender = 'female';
+            const chartUrl = `${ENDPOINTS.GET_CHART.replace('{gender}', gender)}?gender=${gender}`;
+            const [idols, donations, chart] = await Promise.all([
+              fetchData(ENDPOINTS.GET_IDOLS),
+              fetchData(ENDPOINTS.GET_DONATIONS),
+              fetchData(chartUrl),
+            ]);
+            return { idols, donations, chart };
+          },
+          errorElement: <ErrorBoundary />,
+        },
+        {
+          path: 'mypage',
+          element: <MyPage />,
+          loader: async () => {
+            const idols = await fetchData(ENDPOINTS.GET_IDOLS);
+            return idols;
+          },
+          errorElement: <ErrorBoundary />,
+        },
+        {
+          path: 'test-error',
+          element: <div>서버 에러 테스트 페이지입니다</div>,
+          loader: async () => {
+            throw new Response('서버에 문제가 발생했습니다. 잠시 후 다시 시도해주세요.', {
+              status: 500,
+              statusText: 'Internal Server Error',
+            });
+          },
+        },
+        {
+          path: '*',
+          element: <NotFoundPage />,
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: (
-      <PendingUI>
-        <App />
-      </PendingUI>
-    ),
-    errorElement: <ErrorBoundary />,
-    children: [
-      {
-        index: true,
-        element: (
-          <PendingUI>
-            <LandingPage />
-          </PendingUI>
-        ),
-      },
-      {
-        path: 'list',
-        element: (
-          <PendingUI>
-            <ListPage />
-          </PendingUI>
-        ),
-        loader: async () => {
-          const gender = 'female';
-          const chartUrl = `${ENDPOINTS.GET_CHART.replace('{gender}', gender)}?gender=${gender}`;
-          const [idols, donations, chart] = await Promise.all([
-            fetchData(ENDPOINTS.GET_IDOLS),
-            fetchData(ENDPOINTS.GET_DONATIONS),
-            fetchData(chartUrl),
-          ]);
-          return { idols, donations, chart };
-        },
-        errorElement: <ErrorBoundary />,
-      },
-      {
-        path: 'mypage',
-        element: (
-          <PendingUI>
-            <MyPage />
-          </PendingUI>
-        ),
-        loader: async () => {
-          const idols = await fetchData(ENDPOINTS.GET_IDOLS);
-          return idols;
-        },
-        errorElement: <ErrorBoundary />,
-      },
-      {
-        path: 'test-error',
-        element: (
-          <PendingUI>
-            <div>서버 에러 테스트 페이지입니다</div>
-          </PendingUI>
-        ),
-        loader: async () => {
-          throw new Response('서버에 문제가 발생했습니다. 잠시 후 다시 시도해주세요.', {
-            status: 500,
-            statusText: 'Internal Server Error',
-          });
-        },
-      },
-      {
-        path: '*',
-        element: <NotFoundPage />,
-      },
-    ],
+    future: {
+      v7_pendingElement: true,
+    },
   },
-]);
+);
 
 export default router;
