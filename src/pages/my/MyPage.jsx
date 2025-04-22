2706743456d21621c05f1749a6ac0b1abb5f64dd
sonsujin <sonsj33@gmail.com>
2025-04-22 12:44:18 +0900

    ✨Feat: 마이페이지 기초 ui 작업
    # 커밋 메시지 형식
    # <이모지><타입>: <제목> (#이슈 번호)
    #
    # ⛔ 제목은 100자 이내, 끝에 마침표(.) 사용 금지
    # ⛔ 이모지와 타입 사이에 공백 없이 붙여 씁니다 (예: ✨Feat)
    # ⛔ 제목과 콜론(:) 사이에 공백 없이 붙이고, 콜론 뒤에는 공백 1칸 필요
    
    # ✅ 제목은 커밋의 "무엇"을 나타냅니다
    # ✅ 본문은 "왜", "어떻게"를 요약하며, 선택사항입니다
    # ✅ 본문은 여러 줄의 메시지를 작성할 땐 "-"로 구분
    
    # 예시:
    # ✨Feat: 마이페이지 수정 기능 추가 (#15)
    
    # 커밋 타입 가이드:
    # ✨ Feat     : 새로운 기능 추가
    # 🐛 Fix      : 버그 수정
    # 📦 Chore    : 설정/빌드 수정, 기타 잡일
    # 💄 Style    : 포맷팅, 세미콜론 등 비즈니스 로직 없는 변경
    # 📝 Docs     : 문서 작성 및 수정
    # ♻️ Refactor : 리팩토링 (동작 변경 없이 코드 개선)

--- src/pages/my/MyPage.jsx
+++ src/pages/my/MyPage.jsx
@@ -2,7 +2,6 @@ import addIcon from '@/assets/icons/plus-icon.png';
 import Avatar from '@/components/avatar';
 import AvatarButton from '@/components/avatarButton';
 import CustomButton from '@/components/customButton';
-import mockData from '@/data/mockData';
 import { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import * as S from './myPage.styles';
@@ -16,15 +15,9 @@ const MyPage = () => {
     setMyIdol([...myIdol, idol]);
   };
 
-  // useEffect(() => {
-  //   allIdols.filter((idol, index, arr) => {
-  //     return !myIdol.some((selected) => selected.id === idol.id);
-  //   });
-  // }, [myIdol, allIdols]);
-
   return (
     <div css={S.myPageWrapper}>
-      <h2>내가 관심있는 아이돌</h2>
+      <h2 css={S.title}>내가 관심있는 아이돌</h2>
       <section css={S.horizonList}>
         {myIdol.map((idol) => {
           return (
@@ -37,21 +30,23 @@ const MyPage = () => {
         })}
       </section>
       <h2 css={S.title}>관심 있는 아이돌을 추가해보세요.</h2>
-      <section css={S.IdolList}>
-        {allIdols.map((idol) => {
-          return (
-            <div key={idol.id} css={S.allProfileSize}>
-              <Avatar imgUrl={idol.profilePicture} onSelectToggle={() => addMyIdol(idol)} />
-              <h3 css={S.idolName}>{idol.name}</h3>
-              <p css={S.groupName}>{idol.group}</p>
-            </div>
-          );
-        })}
-      </section>
-      <CustomButton type="button" isRound={true} style={S.customButtonStyle}>
-        <img src={addIcon} alt="addIcon" css={S.buttonIcon} />
-        추가하기
-      </CustomButton>
+      <div css={S.idolListWrapper}>
+        <section css={S.idolList}>
+          {allIdols.map((idol) => {
+            return (
+              <div key={idol.id} css={S.allProfileSize}>
+                <Avatar imgUrl={idol.profilePicture} onSelectToggle={() => addMyIdol(idol)} />
+                <h3 css={S.idolName}>{idol.name}</h3>
+                <p css={S.groupName}>{idol.group}</p>
+              </div>
+            );
+          })}
+        </section>
+        <CustomButton type="button" isRound={true} style={S.customButtonStyle}>
+          <img src={addIcon} alt="addIcon" css={S.buttonIcon} />
+          추가하기
+        </CustomButton>
+      </div>
     </div>
   );
 };
