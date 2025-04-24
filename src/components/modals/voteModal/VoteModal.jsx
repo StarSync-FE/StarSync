9b3ef7330ea149a3effa446a70ea93ac59a480f5
sonsujin <sonsj33@gmail.com>
2025-04-25 05:03:50 +0900

    ✨Feat: 투표하기 기능 추가
    # 커밋 메시지 형식
    # <이모지><타입>: <제목> (#이슈 번호)
    #
    # ⛔ 제목은 100자 이내, 끝에 마침표(.) 사용 금지
    # ⛔ 이모지와 타입 사이에 공백 없이 붙여 씁니다 (예: ✨Feat)
    # ⛔ 제목과 콜론(:) 사이에 공백 없이 붙이고, 콜론 뒤에는 공백 1칸 필요
    
    # ✅ 제목은 커밋의 "무엇"을 나타냅니다
    # ✅ 본문은 "왜", "어떻게"를 요약하며, 선택사항입니다
    # ✅ 본문은 여러 줄의 메시지를 작성할 땐 "-"로 구분
    
    # 예시:
    # ✨Feat: 마이페이지 수정 기능 추가 (#15)
    
    # 커밋 타입 가이드:
    # ✨ Feat     : 새로운 기능 추가
    # 🐛 Fix      : 버그 수정
    # 📦 Chore    : 설정/빌드 수정, 기타 잡일
    # 💄 Style    : 포맷팅, 세미콜론 등 비즈니스 로직 없는 변경
    # 📝 Docs     : 문서 작성 및 수정
    # ♻️ Refactor : 리팩토링 (동작 변경 없이 코드 개선)

--- src/components/modals/voteModal/VoteModal.jsx
+++ src/components/modals/voteModal/VoteModal.jsx
@@ -2,29 +2,49 @@ import Avatar from '@/components/avatar/Avatar';
 import CustomButton from '@/components/customButton';
 import RadioButton from '@/components/radioButton/RadioButton';
 import { ENDPOINTS } from '@/constants/api';
-import { requestGet } from '@/utils/api';
+import { requestGet, requestPost } from '@/utils/api';
 import { useEffect, useState } from 'react';
 import * as S from './voteModal.styles';
 
-const VoteModal = ({ gender }) => {
+const VoteModal = ({ gender, updateCredit }) => {
   const [idols, setIdols] = useState([]);
+  const [selectedIdol, setSelectedIdol] = useState();
+  const [checkedItem, setCheckedItem] = useState();
   const loadData = async (gender) => {
     const chartUrl = `${ENDPOINTS.GET_CHART}?gender=${gender}&pageSize=30&`;
     const response = await requestGet(chartUrl);
     return response;
   };
-
+  const voteForIdol = async (idolId) => {
+    const voteUrl = `${ENDPOINTS.REGISTER_VOTE}`;
+    try {
+      const response = await requestPost(voteUrl, { idolId: idolId });
+      if (response) {
+        alert('투표에 성공했습니다');
+        const getCredit = localStorage.getItem('selectedCredit');
+        if (getCredit < 1000) {
+          alert('크레딧이 부족합니다!');
+        } else {
+          localStorage.setItem('selectedCredit', Number(getCredit) - 1000);
+          updateCredit(Number(getCredit) - 1000);
+        }
+      } else {
+        console.log(response);
+      }
+    } catch (err) {
+      console.log('error발생');
+    }
+  };
+  console.log(localStorage.getItem('selectedCredit'));
   // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   useEffect(() => {
     if (!gender) return;
-
     const load = async () => {
       const result = await loadData(gender === 'females' ? 'female' : 'male');
       setIdols(result.idols);
     };
     load();
   }, [gender]);
-  console.log(idols);
   return (
     <div css={S.ModalWrapper}>
       <h2 css={S.title}>이달의 여자 아이돌</h2>
@@ -37,10 +57,16 @@ const VoteModal = ({ gender }) => {
                     style={S.voteRadioButton}
                     itemLabel={'voteGirlsIdol'}
                     name={`voteGirlsIdol-${idol.name}`}
-                    onSelect={() => setIsSelected(true)}
+                    handleSelect={() => {
+                      setCheckedItem(idol.id);
+                    }}
                   >
                     <div css={S.memberInfoBox}>
-                      <Avatar imgUrl={idol.profilePicture} style={S.avatar} />
+                      <Avatar
+                        imgUrl={idol.profilePicture}
+                        style={S.avatar}
+                        isSelected={checkedItem === idol.id}
+                      />
                       <span css={S.rankNumber}>{idol.rank}</span>
                       <div css={S.voteBox}>
                         <p css={S.idolName}>{idol.name}</p>
@@ -53,7 +79,12 @@ const VoteModal = ({ gender }) => {
             })
           : console.log('idols가 없음')}
       </div>
-      <CustomButton style={S.buttonStyle}>투표하기</CustomButton>
+      <CustomButton
+        style={S.buttonStyle}
+        onClick={() => (checkedItem ? voteForIdol(checkedItem) : null)}
+      >
+        투표하기
+      </CustomButton>
       <p css={S.guideQuote}>
         투표하는 데 <b css={S.highlightText}>1000 크레딧</b>이 소요됩니다.
       </p>
