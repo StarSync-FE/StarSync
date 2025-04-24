5570ecca4eedf54cf7e590a807234540010efe48
sonsujin <sonsj33@gmail.com>
2025-04-25 00:42:22 +0900

    ✨Feat: gender에 맞게 데이터 불러오기
    # 커밋 메시지 형식
    # <이모지><타입>: <제목> (#이슈 번호)
    #
    # ⛔ 제목은 100자 이내, 끝에 마침표(.) 사용 금지
    # ⛔ 이모지와 타입 사이에 공백 없이 붙여 씁니다 (예: ✨Feat)
    # ⛔ 제목과 콜론(:) 사이에 공백 없이 붙이고, 콜론 뒤에는 공백 1칸 필요
    
    # ✅ 제목은 커밋의 "무엇"을 나타냅니다
    # ✅ 본문은 "왜", "어떻게"를 요약하며, 선택사항입니다
    # ✅ 본문은 여러 줄의 메시지를 작성할 땐 "-"로 구분
    
    # 예시:
    # ✨Feat: 마이페이지 수정 기능 추가 (#15)
    
    # 커밋 타입 가이드:
    # ✨ Feat     : 새로운 기능 추가
    # 🐛 Fix      : 버그 수정
    # 📦 Chore    : 설정/빌드 수정, 기타 잡일
    # 💄 Style    : 포맷팅, 세미콜론 등 비즈니스 로직 없는 변경
    # 📝 Docs     : 문서 작성 및 수정
    # ♻️ Refactor : 리팩토링 (동작 변경 없이 코드 개선)

--- src/components/modals/voteModal/VoteModal.jsx
+++ src/components/modals/voteModal/VoteModal.jsx
@@ -1,43 +1,58 @@
 import Avatar from '@/components/avatar/Avatar';
 import RadioButton from '@/components/radioButton/RadioButton';
+import { ENDPOINTS } from '@/constants/api';
 import { mockData } from '@/data/mockData';
-import { useState } from 'react';
+import { requestGet } from '@/utils/api';
+import { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router';
 import * as S from './voteModal.styles';
 
-const VoteModal = ({ onClose }) => {
-  const [currentItem, setcurrentItem] = useState({});
-  const selectItem = (e) => {
-    console.log(e.target.value);
+const VoteModal = ({ gender = 'female' }) => {
+  const [idols, setIdols] = useState([]);
+  const loadData = async (gender) => {
+    const chartUrl = `${ENDPOINTS.GET_CHART}?gender=${gender}&pageSize=30&`;
+    const response = await requestGet(chartUrl);
+    return response;
   };
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  useEffect(() => {
+    if (!gender) return;
 
+    const load = async () => {
+      const result = await loadData(gender === 'girls' ? 'female' : 'male');
+      setIdols(result.idols);
+      console.log(`${gender === 'girls' ? '여자' : '남자'} 아이돌`, result.idols);
+    };
+    load();
+  }, [gender]);
   return (
     <div css={S.ModalWrapper}>
-      {mockData.girls.map((girl) => {
-        return (
-          <RadioButton
-            style={S.voteRadioButton}
-            key={girl.name}
-            itemLabel={'voteGirlsIdol'}
-            name={`voteGirlsIdol-${girl.name}`}
-            onSelect={() => setIsSelected(true)}
-          >
-            <div css={S.memberInfoBox}>
-              <Avatar imgUrl={girl.img} />
-              <span css={S.rankNumber}>{girl.rank}</span>
-              <div css={S.voteBox}>
-                <p css={S.idolName}>{girl.name}</p>
-                <p css={S.votes}>{girl.votes}표</p>
-              </div>
-            </div>
-          </RadioButton>
-        );
-      })}
-      <button type="button" onClick={onClose}>
-        투표하기
-      </button>
+      {/* {idols.length > 0
+        ? idols.map((idol) => {
+            return (
+              <RadioButton
+                style={S.voteRadioButton}
+                key={idol.name}
+                itemLabel={'voteGirlsIdol'}
+                name={`voteGirlsIdol-${idol.name}`}
+                onSelect={() => setIsSelected(true)}
+              >
+                <div css={S.memberInfoBox}>
+                  <Avatar imgUrl={idol.img} />
+                  <span css={S.rankNumber}>{idol.rank}</span>
+                  <div css={S.voteBox}>
+                    <p css={S.idolName}>{idol.name}</p>
+                    <p css={S.votes}>{idol.votes}표</p>
+                  </div>
+                </div>
+              </RadioButton>
+            );
+          })
+        : console.log('idols가 없음')}
+      <button type="button">투표하기</button>
       <p css={S.guideQuote}>
         투표하는 데 <b css={S.highlightText}>1000 크레딧</b>이 소요됩니다.
-      </p>
+      </p> */}
     </div>
   );
 };
