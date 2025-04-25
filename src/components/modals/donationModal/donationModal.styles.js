78a27541680fc5479f72c88f44ec0f6378bdc7d7
Jo Insung <leeming01@naver.com>
2025-04-25 23:50:05 +0900

    💄Style: 후원 모달 스타일 구현
    # 커밋 메시지 형식
    # <이모지><타입>: <제목> (#이슈 번호)
    #
    # ⛔ 제목은 100자 이내, 끝에 마침표(.) 사용 금지
    # ⛔ 이모지와 타입 사이에 공백 없이 붙여 씁니다 (예: ✨Feat)
    # ⛔ 제목과 콜론(:) 사이에 공백 없이 붙이고, 콜론 뒤에는 공백 1칸 필요
    
    # ✅ 제목은 커밋의 "무엇"을 나타냅니다
    # ✅ 본문은 "왜", "어떻게"를 요약하며, 선택사항입니다
    # ✅ 본문은 여러 줄의 메시지를 작성할 땐 "-"로 구분
    
    # 예시:
    # ✨Feat: 마이페이지 수정 기능 추가 (#15)
    
    # 커밋 타입 가이드:
    # ✨ Feat     : 새로운 기능 추가
    # 🐛 Fix      : 버그 수정
    # 📦 Chore    : 설정/빌드 수정, 기타 잡일
    # 💄 Style    : 포맷팅, 세미콜론 등 비즈니스 로직 없는 변경
    # 📝 Docs     : 문서 작성 및 수정
    # ♻️ Refactor : 리팩토링 (동작 변경 없이 코드 개선)

--- src/components/modals/donationModal/donationModal.styles.js
+++ src/components/modals/donationModal/donationModal.styles.js
@@ -1,16 +1,93 @@
 import { css } from '@emotion/react';
 
-export const radioButtonContent = css`
+export const modalContent = css`
   display: flex;
+  flex-direction: column;
   align-items: center;
-  gap: 0.5rem;
+  gap: 24px;
+  width: 32.7rem;
+  height: 49.4rem;
+  padding-top: 2.4rem;
 
-  img {
-    width: 1rem;
-    height: 1rem;
+  h2 {
+    align-self: flex-start;
+    padding-left: 1.6rem;
+    font-size: 1.8rem;
+    font-weight: 600;
   }
 
-  span {
+  figure > img {
+    width: 15.8rem;
+    height: 20.6rem;
+    border-radius: 8px;
+    object-fit: cover;
+    object-position: center;
+  }
+`;
+
+export const wrapper = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+
+  img {
+    align-self: center;
+  }
+
+  & > div {
+    display: flex;
+    flex-direction: column;
+    gap: 6px;
+    width: 15.4rem;
+  }
+`;
+
+export const titleContent = css`
+  p {
+    font-size: 1.2rem;
+    color: var(--gray);
+  }
+
+  h3 {
+    /* 디자인을 위해 필요할까? */
+    overflow: hidden;
+    font-size: 1.4rem;
+    font-weight: 500;
+    white-space: nowrap;
+    color: var(--white-full);
+    text-overflow: ellipsis;
+}
+`;
+
+export const inputContent = css`
+  position: relative;
+  width: 29.5rem;
+  height: 5.8rem;
+
+  input {
+    width: 100%;
+    height: 100%;
+    padding: 1.6rem;
+    border: 1px solid var(--white-full);
+    border-radius: 8px;
+    font-size: 2rem;
     font-weight: 700;
+    background-color: #272f3d;
+  }
+
+  input::placeholder {
+    font-size: 2rem;
+    font-weight: 700;
+    text-align: left;
+  }
+
+  img {
+    position: absolute;
+    top: 50%;
+    right: 5%;
+    width: 3.6rem;
+    height: 3.6rem;
+    transform: translateY(-50%);
   }
 `;
